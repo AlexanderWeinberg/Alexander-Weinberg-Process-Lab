4d1a6e0ce1a58e708415d1d5627e95d673dbf66f
Alexander Weinberg <alexw.dagreat@gmail.com>
2020-09-25 22:10:35 +0000

    Fixed status position

--- Processes/processes.c
+++ Processes/processes.c
@@ -8,25 +8,32 @@
   void main(int argc, char *arg[]){
   srand(time(NULL));
     int pid;
-    int status = wait(&status);
+    
     int i;
     int x;
-    int sleeping = (1 +  rand()%10);
-    int looping = (1 +  rand()%30);
+    
     
     for(i=0;i<MAX; i++){  //NEVER use while(1)  (while true) => infinite loop
       
       pid = fork();  //creates a child 
       
     if (pid ==0){
-     for(x = 0; x < looping; x++){
+      
+      
+    int sleeping = (1 +  rand()%10);//sleeping for at most 10 seconds
+    int looping = (1 +  rand()%30); // looping for at most 30 iterations
+     
+      for(x = 0; x < looping; x++){
        printf("Child Pid: <%d> is going to sleep!\n",getpid());
        sleep(sleeping);
        printf("Child Pid: <%d> is awake!\nWhere is my Parent: <%d>?\n",getpid(),getppid());
      } exit(0); 
     }
      else if(pid > 0){//parent process
+       int status = wait(&status);
+       printf("\n");
        printf("Child Pid: <%d> has completed\n",status);
+       printf("\n");
        }
       else{
        printf("An ERROR Occured\n");
